fed704dd674fade951151f6f36520eac57abad41
NashibJr <nashibjr@gmail.com>
2023-03-25 12:02:04 -0700

    Removing the unnecessary files

--- src/components/recipe.js
+++ src/components/recipe.js
@@ -1,4 +1,4 @@
-import React, { createRef } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const Recipe = ({ recipe }) => {
--- src/redux/app/store.js
+++ src/redux/app/store.js
@@ -1,12 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { apiSlice } from "../../features/app/apiSlice";
 import recipeSlice from "./recipeSlice";
 
 export const store = configureStore({
   reducer: {
     recipes: recipeSlice.reducer,
-    [apiSlice.reducerPath]: apiSlice.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
 });
